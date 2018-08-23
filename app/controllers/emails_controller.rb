bc97c9943a998da5955f72db8726ca63a94eac69
willy <wilg1569@gmail.com>
2018-08-23 21:36:10 +0200

    Delete mail

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -13,5 +13,10 @@ class EmailsController < ApplicationController
 
   def destroy
     @emails = Email.find(params[:id])
+    @emails.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'emails#index'
-  get 'show/:id', to: 'emails#show'
+  get 'mail/:id', to: 'emails#show'
+  delete 'mail/:id', to: 'emails#destroy'
 end
