471d1a3fb94a56c209c19ab1fd6fae2aab0a9596
willy <wilg1569@gmail.com>
2018-08-23 20:13:11 +0200

    Affiche corps d'un mail au clic

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,12 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+
+  def show
+    @emails = Email.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
+  end
 end
