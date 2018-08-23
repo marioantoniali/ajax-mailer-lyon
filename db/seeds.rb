ee04c1fff73536c022d4a5237f307abe20714763
willy <wilg1569@gmail.com>
2018-08-23 14:24:31 +0200

    Creation model and database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Email.create([{object: "THP Project", body: "The purpose of our school is to teach programming with a new approach"}, {object: "New movie", body: "Check out this movie, it's going to be released on Friday"}, {object: "Avengers", body: "The Hulk is the strongest Avenger of all, I don't care if Thor is a god my god is The Hulk"}, {object: "Batman & Robin", body: "Batman is the best, Damian is a pain in the ass"}, {object: "Kirikou", body: "Karaba is mean but in the end she overcomes her fears and Kirikou is happy. The end"}])
